3e1d02b33a2de00ed5ecfb066258bb3424a80909
Christopher Little <christopher.little.personal@gmail.com>
2025-01-12 10:32:20 +0000

    Checkpoint

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,18 +7,18 @@ const NavBar: React.FC = () => {
       <div className="container mx-auto px-6 py-3">
         <div className="md:flex md:items-center md:justify-between">
           <div className="flex justify-between items-center">
-            <div className="text-xl font-semibold text-gray-700">
-              <a href="/" className="hover:text-gray-900 transition duration-150">
+            <div className="text-xl font-semibold">
+              <a href="/" className="transition duration-150">
                 Christopher Little
               </a>
             </div>
           </div>
           <div className="flex items-center mt-4 md:mt-0">
-            <Link href="/" className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-            <Link href="/data-engineering" className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Data Engineering</Link>
-            <Link href="/software-engineering" className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Software Engineering</Link>
-            <Link href="/machine-learning" className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Machine Learning</Link>
-            <Link href="/python-packages" className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Python Packages</Link>
+            <Link href="/" className="px-3 py-2 rounded-md text-sm font-medium">Home</Link>
+            <Link href="/data-engineering" className="px-3 py-2 rounded-md text-sm font-medium">Data Engineering</Link>
+            <Link href="/software-engineering" className="px-3 py-2 rounded-md text-sm font-medium">Software Engineering</Link>
+            <Link href="/machine-learning" className="px-3 py-2 rounded-md text-sm font-medium">Machine Learning</Link>
+            <Link href="/python-packages" className="px-3 py-2 rounded-md text-sm font-medium">Python Packages</Link>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Image from 'next/image';
 import Link from 'next/link';
-import { ArrowPathIcon, CogIcon, CubeIcon, DevicePhoneMobileIcon, DocumentTextIcon, LinkIcon, CodeBracketIcon } from '@heroicons/react/24/outline';
+import { DocumentTextIcon, LinkIcon, CodeBracketIcon, CpuChipIcon, CogIcon, CircleStackIcon } from '@heroicons/react/24/outline';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen">
       <NavBar />
       <header className="flex flex-col items-center justify-center pt-20">
         <Image src="/images/me.jpeg" alt="My Photo" width={150} height={150} className="rounded-full mb-8" />
-        <h1 className="text-4xl font-bold mb-8 text-white">Chris Little — Data Engineer</h1>
+        <h1 className="text-4xl font-bold mb-8">Christopher Little — Data Engineer</h1>
 
         <p className="text-lg mb-20">Welcome to my personal portfolio site.</p>
         <div className="grid grid-cols-3 gap-x-24 mb-24 w-full max-w-2xl mx-auto">
-          <a href="https://linkedin.com" className="hover:text-blue-400 transition flex flex-col items-center">
-            <LinkIcon className="h-8 w-8 mb-4 text-blue-500" />
-            <span className="text-2xl font-mono text-white">LinkedIn</span>
+          <a href="https://linkedin.com" className="hover:opacity-75 transition flex flex-col items-center">
+            <LinkIcon className="h-8 w-8 mb-4 text-red-600" />
+            <span className="text-xl font-mono">LinkedIn</span>
           </a>
-          <a href="https://github.com" className="hover:text-red-400 transition flex flex-col items-center">
-            <CodeBracketIcon className="h-8 w-8 mb-4 text-red-500" />
-            <span className="text-2xl font-mono text-white">GitHub</span>
+          <a href="https://github.com" className="hover:opacity-75 transition flex flex-col items-center">
+            <CodeBracketIcon className="h-8 w-8 mb-4 text-red-600" />
+            <span className="text-xl font-mono">GitHub</span>
           </a>
-          <a href="/cv" className="hover:text-orange-400 transition flex flex-col items-center">
-            <DocumentTextIcon className="h-8 w-8 mb-4 text-orange-500" />
-            <span className="text-2xl font-mono text-white">CV</span>
+          <a href="/cv" className="hover:opacity-75 transition flex flex-col items-center">
+            <DocumentTextIcon className="h-8 w-8 mb-4 text-red-600" />
+            <span className="text-xl font-mono">CV</span>
           </a>
         </div>
       </header>
@@ -34,35 +34,35 @@ const HomePage: React.FC = () => {
             src="/images/colours.png" 
             alt="Colorful Background" 
             fill 
-            className="blur-xl" 
+            className="blur-[32px]" 
           />
         </div>
         <div className="relative z-10 max-w-5xl mx-auto p-6">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-            <Link href="/data-engineering" className="bg-gray-800/80 backdrop-blur-sm shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
-              <ArrowPathIcon className="h-8 w-8 mb-2 text-white" />
-              <h2 className="text-xl font-semibold text-white">Data Engineering</h2>
-              <p className="text-center text-gray-300">Building scalable data pipelines and infrastructure</p>
+            <Link href="/data-engineering" className="bg-black shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
+              <CircleStackIcon className="h-8 w-8 mb-2 text-purple-600" />
+              <h2 className="text-xl font-semibold mb-2">Data Engineering</h2>
+              <p className="text-center">Scalable data pipelines and infrastructure</p>
             </Link>
-            <Link href="/software-engineering" className="bg-gray-800/80 backdrop-blur-sm shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
-              <CogIcon className="h-8 w-8 mb-2 text-white" />
-              <h2 className="text-xl font-semibold text-white">Software Engineering</h2>
-              <p className="text-center text-gray-300">Full-stack development and system architecture</p>
+            <Link href="/software-engineering" className="bg-black shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
+              <CogIcon className="h-8 w-8 mb-2 text-purple-600" />
+              <h2 className="text-xl font-semibold mb-2">Software Engineering</h2>
+              <p className="text-center">Full-stack development</p>
             </Link>
-            <Link href="/machine-learning" className="bg-gray-800/80 backdrop-blur-sm shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
-              <DevicePhoneMobileIcon className="h-8 w-8 mb-2 text-white" />
-              <h2 className="text-xl font-semibold text-white">Machine Learning</h2>
-              <p className="text-center text-gray-300">ML model development and deployment</p>
+            <Link href="/machine-learning" className="bg-black shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
+              <CpuChipIcon className="h-8 w-8 mb-2 text-purple-600" />
+              <h2 className="text-xl font-semibold mb-2">Machine Learning</h2>
+              <p className="text-center">ML model development and GenAI</p>
             </Link>
-            <Link href="/python-packages" className="bg-gray-800/80 backdrop-blur-sm shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
-              <CubeIcon className="h-8 w-8 mb-2 text-white" />
-              <h2 className="text-xl font-semibold text-white">Python Packages</h2>
-              <p className="text-center text-gray-300">Open-source Python library contributions</p>
+            <Link href="/python-packages" className="bg-black shadow-md p-4 rounded-lg hover:shadow-lg transition max-w-sm mx-auto w-full h-40 flex flex-col items-center justify-center">
+              <CodeBracketIcon className="h-8 w-8 mb-2 text-purple-600" />
+              <h2 className="text-xl font-semibold mb-2">Python Packages</h2>
+              <p className="text-center">Open-source Python library contributions</p>
             </Link>
           </div>
         </div>
       </main>
-      <footer className="bg-black p-4 text-center">
+      <footer className="p-4 text-center">
         <div className="flex justify-center space-x-4 mb-2">
         </div>
       </footer>
